f4486085e86a2d8f8a86ff3266b8aa217fc3e51c
Diksha Jha <dikshajha323>
2026-02-25 10:15:18 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("hello world");
+var num=10;
+    console.log(num);
+    console.log(tyoeof(num));
+    num=false;
+    var person={
+        name: "diksha jha";
+        branch: "ece";
+    }
+    const sym1=symbol(4)
+    console.log(sym1);
+    const sym2= symbol(4);
+    console.log(sym2);
+    if(sym1==sym2){
+        console.log("true");
+    }
+    else{
+        console.log("false");
+    }
