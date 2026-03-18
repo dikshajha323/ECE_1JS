838a80a6a67f6bff237bfc541d01751d46a487ca
Diksha Jha <dikshajha323>
2026-03-18 10:10:08 +0530

    fn

--- dom/script.js
+++ dom/script.js
@@ -1,1 +1,16 @@
-const result=document.getElementById("jk");
+// const result=document.getElementById("jk");
+// const heading=document.createElement("h2");
+// const bodytag=document.getElementsByTagName("body");
+function onClick(){
+    console.log("clicked");
+}
+function onmouseover(){
+    console.log("on mouse out");
+}
+function onmouseout(){
+    console.log("on mouse out")
+}
+function onmousedown(){
+    console.log("on mouse down")
+}
+
